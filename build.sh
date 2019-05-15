1726be7ffec5e95e26621655d49c97b19c41c0d1
Michael Baumgarten <mb2732@rit.edu>
2019-05-15 02:26:14 -0400

    updated .toml and build.sh project names

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cargo build --release
-sudo setcap cap_net_admin=eip target/release/dns-feed
-sudo setcap cap_net_raw=eip target/release/dns-feed
-./target/release/dns-feed
+sudo setcap cap_net_admin=eip target/release/network-analyzer
+sudo setcap cap_net_raw=eip target/release/network-analyzer
+./target/release/network-analyzer
